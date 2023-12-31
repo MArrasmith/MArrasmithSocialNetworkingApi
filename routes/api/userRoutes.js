06b1872d6da2f6244501b992baf99d668f826936
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2023-12-31 17:13:51 -0500

    added user route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,26 @@
 const router = require("express").Router();
 
+const {
+    getUser,
+    getAllUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+router.route('/')
+.get(getAllUser)
+.post(createUser);
+
+router.route('/:userId')
+.get(getUser)
+.put(updateUser)
+.delete(deleteUser);
+
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
 module.exports = router;
